f80d41ba4185acda74653fb51cd3c26f602bfaa5
Dmitry Evdokimov <vlg1evdokimov@gmail.com>
2020-09-11 16:44:59 +0300

    Configuration path for folder Driver (on win)

--- src/test/java/InputNameTest.java
+++ src/test/java/InputNameTest.java
@@ -16,7 +16,7 @@ public class InputNameTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
 
     @BeforeEach
